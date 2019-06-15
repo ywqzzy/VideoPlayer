4a26225ca524294384edbded4ceb84b54f073688
ywqzzy <592838129@qq.com>
2019-06-15 23:13:59 +0800

    default videoplayer

--- app/src/main/java/com/zzywq/videoplayer/MainActivity.java
+++ app/src/main/java/com/zzywq/videoplayer/MainActivity.java
@@ -1,13 +1,48 @@
 package com.zzywq.videoplayer;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (VideoView)findViewById(R.id.videoView);
+        Uri fileUri = Uri.fromFile(new
+                File(Environment.getExternalStorageDirectory().getAbsolutePath()+ "/test.mp4"));
+
+        //String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/test.mp4";
+        /**
+         * 本地视频播放
+       */
+        videoView.setVideoURI(fileUri);
+        /**
+         * 网络播放
+         */
+        //videoView.setVideoURI(Uri.parse(""));
+
+
+        MediaController controller = new MediaController(this);
+        /**
+         * 设置videoview与mediacontroller关联
+         */
+        videoView.setMediaController(controller);
+        /**
+         * 设置mediacontrollerw与videoview关联
+         */
+        controller.setMediaPlayer(videoView);
+
     }
 }
